16c770d4ea9c1852670f3e1a2e558e8e25ebcf0c
amanRaj7 <92115066+amanRaj7@users.noreply.github.com>
2022-03-28 12:41:54 +0530

    Update question_7.c

--- question_7.c
+++ question_7.c
@@ -3,7 +3,7 @@
 int main(){
     int i = 20, j = 30, ans = 0;
     for(; i<=j; i++){
-        if(i%6==0) ans += i;
+        if((i%2==0)&&(i%3==0)) ans += i;
     }
     printf("Sum: %d", ans);
     return 0;
